af0a2d75e48f24378616615f131814e1e9d0d9fb
garciamunoz <garciamunozla@gmail.com>
2016-10-01 20:03:58 -0500

    Estados

---
+++ TP2_Villa/src/com/petcenter/laboratorio/dao/spec/EstadoMapper.java
@@ -0,0 +1,11 @@
+package com.petcenter.laboratorio.dao.spec;
+
+import java.util.List;
+
+import com.petcenter.laboratorio.dto.EstadoDTO;
+
+public interface EstadoMapper {
+	
+	List<EstadoDTO> listaEstados();
+
+}
---
+++ TP2_Villa/src/com/petcenter/laboratorio/dto/EstadoDTO.java
@@ -0,0 +1,43 @@
+package com.petcenter.laboratorio.dto;
+
+public class EstadoDTO {
+
+	private Integer idEstado;
+	private String codigoEstado;
+	private String nombreEstado;
+	private String descripcionEstado;
+	
+	public EstadoDTO(){}
+	
+	public EstadoDTO(Integer idEstado,String codigoEstado,String nombreEstado,String descripcionEstado){
+		this.idEstado = idEstado;
+		this.codigoEstado = codigoEstado;
+		this.nombreEstado = nombreEstado;
+		this.descripcionEstado = descripcionEstado;
+	}
+	public Integer getIdEstado() {
+		return idEstado;
+	}
+	public void setIdEstado(Integer idEstado) {
+		this.idEstado = idEstado;
+	}
+	public String getCodigoEstado() {
+		return codigoEstado;
+	}
+	public void setCodigoEstado(String codigoEstado) {
+		this.codigoEstado = codigoEstado;
+	}
+	public String getNombreEstado() {
+		return nombreEstado;
+	}
+	public void setNombreEstado(String nombreEstado) {
+		this.nombreEstado = nombreEstado;
+	}
+	public String getDescripcionEstado() {
+		return descripcionEstado;
+	}
+	public void setDescripcionEstado(String descripcionEstado) {
+		this.descripcionEstado = descripcionEstado;
+	}
+	
+}
