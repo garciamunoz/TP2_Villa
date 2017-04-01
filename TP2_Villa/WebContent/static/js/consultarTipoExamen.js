678689f00a21359459cfaddfe2f5ed8f77dfd185
garciamunoz <garciamunozla@gmail.com>
2017-04-01 13:14:16 -0500

    ok

---
+++ TP2_Villa/WebContent/static/js/consultarTipoExamen.js
@@ -0,0 +1,30 @@
+$(function(){
+	
+	$('#btnBuscar').click(function(){
+		
+		var nomb=$("#nombre").val();
+		
+		$.ajax({
+			url: "consultarTipoExamen/buscar",
+			async : true,
+			type: "get",
+			data: {nombre: nomb},
+			success: function(result){
+				$("#tbConsultarTipoExamen").empty();
+				$.each(result, function (index, item) {
+					var row = "<tr>" +
+		    		"<td><input type=\"checkbox\" idExamenClinico=\"" + item.idExamenClinico + "\"></td>" +
+		    		"<td>" + item.idExamenClinico + "</td>" +
+		    		"<td>" + item.nombreExamenClinico + "</td>" +
+		    		"<td>" + item.descripcionExamenClinico + "</td>" + 
+		    		"<td>" + item.diasCultivo + "</td>" + 
+		    		"</tr>";
+		    
+					$("#tbConsultarTipoExamen").append(row); 
+				});
+			}
+		});
+	});
+	
+	
+});
